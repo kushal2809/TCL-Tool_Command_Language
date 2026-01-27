053883b5d81c7c2a50b92984727a09a7c26166b2
Koushik D <101798417+kushal2809@users.noreply.github.com>
2026-01-28 00:41:19 +0530

    Create day1.tcl
    This is my day 1 of starting the TCL coding 
    with some basics of TCL

---
+++ day1.tcl
@@ -0,0 +1,7 @@
+puts "hello"
+puts "its my day 1 of learning tcl"
+puts "TCL- Tool Command Language"
+puts "TCL takes commands then reads it line by line then it executes"
+puts "we use expr for math"
+puts [expr 2+3]
+puts [expr 5*4]
